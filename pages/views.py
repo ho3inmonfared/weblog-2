ef91bb460a95fee7c4ded47a13fd61126d0c1f17
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2026-01-03 19:41:03 +0330

    insert posts to home page
    I use Django ORM and get posts model with filter(status='pub' ) and then insert them to my home page  with for loop . I use FBV for view .

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
+from posts import models
 
-class Home(generic.TemplateView):
-    template_name='pages/home.html'
+# class Home(generic.TemplateView):
+#     template_name='pages/home.html'
+
+def homeview(request):
+    posts=models.Post.objects.filter(status='pub').order_by('-created_at')
+    
+    context={
+        'posts':posts
+    }
+    return render(request,'pages/home.html',context)
     
