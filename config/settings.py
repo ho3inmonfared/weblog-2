76acf5b23deaad1c2d75213f506c2befa279e709
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2026-01-04 10:44:33 +0330

    first deploy
    this is for my first deploy:D

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wqg-m_z#0%_vtl(0_k+c_lx0!ggk&4z8*20ync3jm$*j4ommb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -113,6 +113,8 @@ USE_TZ = True
 # Static settings 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # AUth settings 
